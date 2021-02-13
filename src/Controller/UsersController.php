f89c0fb5744eb1be4d945b62b87e96100fd6470d
Alex <savproga@mail.ru>
2021-02-13 16:51:08 +0300

    add login

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsersController extends AbstractController
+{
+    /**
+     * @Route("/users", name="users")
+     */
+    public function index(): Response
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        return $this->render('users/list.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/users/add", name="users_add")
+     * @param  $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return mixed
+     */
+
+    public function create(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $em = $this->getDoctrine()->getManager();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            //$user->setRoles(['ROLE_USER']);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->render('users/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
